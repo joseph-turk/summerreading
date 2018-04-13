26bfeffe4d6935e1b67e4ebb1223192e7da2aeb9
Joseph Turk <joe@Josephs-MacBook-Pro.local>
2018-04-13 15:52:21 -0500

    Tweak login, logout, and header

--- app/admin.py
+++ app/admin.py
@@ -9,3 +9,9 @@ admin.site.register(Program)
 admin.site.register(Adult)
 admin.site.register(Child)
 admin.site.register(Registration)
+
+admin.AdminSite.login_template = 'admin/login.html'
+admin.AdminSite.logout_template = 'admin/logout.html'
+admin.AdminSite.site_header = 'Summer Reading Signup Administration'
+admin.AdminSite.site_title = 'Summer Reading Signup'
+admin.AdminSite.index_title = 'Administration'
