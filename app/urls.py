aa27015fbc2460c855d28fd78d6fda29b41458ed
Joseph Turk <joe@Josephs-MacBook-Pro.local>
2018-04-16 01:43:32 -0500

    Clean up URLs and views

--- app/urls.py
+++ app/urls.py
@@ -8,26 +8,32 @@ from . import views
 app_name = 'app'
 urlpatterns = [
     # Home Views
-    path('', views.HomeView.as_view(), name='home'),
-    path('kids/', views.grid_register_kids, name='kids_home'),
-    path('teens/', views.grid_register_teens, name='teens_home'),
+    path('', views.home, name='home'),
+    path('kids/', views.register_kids, name='kids_home'),
+    path('teens/', views.register_teens, name='teens_home'),
     # Program Views
-    path('kids/programs/', views.KidProgramsIndex.as_view(), name='kid_programs'),
-    path('teens/programs/', views.TeenProgramsIndex.as_view(), name='teen_programs'),
-    path('kids/programs/<int:pk>', views.DetailView.as_view(),
+    path('kids/programs/',
+         views.KidProgramsIndex.as_view(),
+         name='kid_programs'),
+    path('teens/programs/',
+         views.TeenProgramsIndex.as_view(),
+         name='teen_programs'),
+    path('kids/programs/<int:pk>',
+         views.ProgramDetail.as_view(),
          name="kids_program_detail"),
-    path('teens/programs/<int:pk>', views.DetailView.as_view(),
+    path('teens/programs/<int:pk>',
+         views.ProgramDetail.as_view(),
          name='teen_program_detail'),
     # Registration Views
-    path('kids/register/', views.grid_register_kids, name='grid_register_kids'),
-    path('teens/register/', views.grid_register_teens, name='grid_register_teens'),
-    path('register/add_registration', views.add_grid_registration,
-         name='add_grid_registration'),
-    path('register/confirmation/<uuid:pk>', views.grid_confirmation,
-         name='grid_confirmation'),
+    path('register/add_registration',
+         views.add_registration,
+         name='add_registration'),
+    path('register/confirmation/<uuid:pk>',
+         views.confirmation,
+         name='confirmation'),
     # Admin-Only Views
-    path('programs/', views.home, name='programs'),
-    path('programs/<int:pk>', views.DetailView.as_view(), name='detail'),
+    path('programs/', views.ProgramList.as_view(), name='programs'),
+    path('programs/<int:pk>', views.ProgramDetail.as_view(), name='detail'),
     path('patrons/', views.patrons, name='patrons'),
     path('patrons/<uuid:pk>', views.patron_detail, name='patron')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -12,34 +12,17 @@ from .models import Child
 from .models import Registration
 
 
+# GET: /
 def home(request):
-    programs = get_list_or_404(Program)
-    return render(request, 'programs/index.html', {'programs': programs})
+    return render(request, 'home/index.html')
 
 
-class HomeView(generic.ListView):
-    template_name = 'home/index.html'
-    context_object_name = 'programs'
-
-    def get_queryset(self):
-        '''Get next three programs.'''
-        return Program.objects.exclude(date__lt=datetime.today()).order_by('date')[:5]
-
-
-def kids_home(request):
-    return render(request, 'home/kids.html')
-
-
-def teens_home(request):
-    return render(request, 'home/teens.html')
-
-
-class IndexView(generic.ListView):
+# GET: /programs
+class ProgramList(generic.ListView):
     template_name = 'programs/index.html'
     context_object_name = 'programs'
 
     def get_queryset(self):
-        '''Return all programs.'''
         return Program.objects.order_by('date')
 
 
@@ -61,27 +44,12 @@ class TeenProgramsIndex(generic.ListView):
         return Program.objects.exclude(is_teen=False).order_by('date')
 
 
-class DetailView(generic.DetailView):
+class ProgramDetail(generic.DetailView):
     model = Program
     template_name = 'programs/detail.html'
 
 
-def register(request, program_id):
-    program = get_object_or_404(Program, pk=program_id)
-    return render(request, 'programs/register.html', {'program': program})
-
-
-def grid_register(request):
-    programs = get_list_or_404(Program)
-    return render(request,
-                  'registrations/create.html',
-                  {
-                      'programs': programs,
-                      'type': 'teens'
-                  })
-
-
-def grid_register_kids(request):
+def register_kids(request):
     programs = get_list_or_404(Program.objects.order_by(
         'date'), is_teen=False, date__gt=datetime.today())
     return render(request,
@@ -92,7 +60,7 @@ def grid_register_kids(request):
                   })
 
 
-def grid_register_teens(request):
+def register_teens(request):
     programs = get_list_or_404(
         Program, is_teen=True, date__gt=datetime.today())
     return render(request,
@@ -103,7 +71,7 @@ def grid_register_teens(request):
                   })
 
 
-def add_grid_registration(request):
+def add_registration(request):
     programs = []
 
     adult, created = Adult.objects.get_or_create(
@@ -135,50 +103,8 @@ def add_grid_registration(request):
                     program.is_full = True
                     program.save()
 
-    return HttpResponseRedirect(reverse('app:grid_confirmation',
+    return HttpResponseRedirect(reverse('app:confirmation',
                                         args=[adult.id]))
-
-
-def add_registration(request, program_id):
-    program = get_object_or_404(Program, pk=program_id)
-
-    wait_list = False
-    if program.is_full:
-        wait_list = True
-
-    adult, created = Adult.objects.get_or_create(
-        name=request.POST['adultname'],
-        email=request.POST['adultemail']
-    )
-
-    for key in request.POST:
-        if key.startswith('childname'):
-            child, created = Child.objects.get_or_create(
-                name=request.POST[key],
-                adult=adult
-            )
-
-            registration, created = Registration.objects.get_or_create(
-                program=program,
-                child=child,
-                is_wait_list=wait_list
-            )
-
-    if program.registration_set.count() >= program.capacity:
-        program.is_full = True
-        program.save()
-
-    return HttpResponseRedirect(reverse('app:confirmation', args=(program.id,)))
-
-
-# Registration Views
-# ------------------
-
-class RegistrationCreate(generic.edit.CreateView):
-    '''Single Program Registration'''
-    model = Registration
-    success_url = reverse_lazy('programs')
-    fields = ['program', 'child', 'is_wait_list']
 
 
 @login_required(login_url='/admin/login/')
@@ -193,11 +119,6 @@ def patron_detail(request, pk):
     return render(request, 'patrons/detail.html', {'patron': patron})
 
 
-def confirmed(request, program_id):
-    program = get_object_or_404(Program, pk=program_id)
-    return render(request, 'programs/confirmation.html', {'program': program})
-
-
-def grid_confirmation(request, pk):
+def confirmation(request, pk):
     patron = get_object_or_404(Adult, pk=pk)
     return render(request, 'registrations/confirmation.html', {'patron': patron})
