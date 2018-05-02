edd85fbdf982e5829bcf18c540f586c3308459f8
Joseph Turk <joseph.r.turk@gmail.com>
2018-05-02 18:05:48 -0500

    Only allow submission of registration if a program is selected

---
+++ app/static/shared/validateCheckboxes.js
@@ -0,0 +1,7 @@
+const requiredCheckboxes = $(":checkbox[required]");
+
+requiredCheckboxes.change(() => {
+  if (requiredCheckboxes.is(":checked"))
+    requiredCheckboxes.removeAttr("required");
+  else requiredCheckboxes.attr("required", "required");
+});
